7dc4520216fad1eb8a2cc03179ff792eaee16174
kaurjap9 <japnoor.kaur@mail.utoronto.ca>
2025-10-30 00:05:10 -0400

    clean up paint panel and view after shapechooserpanel

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/View.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/View.java
@@ -1,133 +1,66 @@
 package ca.utoronto.utm.assignment2.paint;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class View implements EventHandler<ActionEvent> {
+public class View {
 
-        private PaintModel paintModel;
-        private PaintPanel paintPanel;
-        private ShapeChooserPanel shapeChooserPanel;
+        private final PaintModel paintModel;
+        private final PaintPanel paintPanel;
 
-        public View(PaintModel model, Stage stage) {
-            this.paintModel = model;
-
-            this.paintPanel = new PaintPanel(this.paintModel);
-            this.shapeChooserPanel = new ShapeChooserPanel(this);
-
-            BorderPane root = new BorderPane();
-            root.setTop(createMenuBar());
-            root.setCenter(this.paintPanel);
-            root.setLeft(this.shapeChooserPanel);
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.setTitle("Paint");
-            stage.show();
+    public View(PaintModel model, Stage stage) {
+                this.paintModel = model;
+                this.paintPanel = new PaintPanel(this.paintModel);
+        ShapeChooserPanel shapeChooserPanel = new ShapeChooserPanel(this);
+                BorderPane root = new BorderPane();
+                root.setTop(createMenuBar());
+                root.setCenter(this.paintPanel);
+                root.setLeft(shapeChooserPanel);
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setTitle("Paint");
+                stage.show();
         }
 
-        public PaintModel getPaintModel() {
-                return this.paintModel;
-        }
+        public PaintModel getPaintModel() { return this.paintModel; }
         public PaintPanel getPaintPanel() { return this.paintPanel; }
 
-        public void setMode(String mode) {
-                ToolStrategy strategy = null;
-
-                // Select the correct strategy based on button click
-                switch (mode) {
-                        case "Circle" -> strategy = new CircleStrategy(paintModel, paintPanel);
-                        case "Rectangle" -> strategy = new RectangleStrategy(paintModel, paintPanel);
-                        case "Squiggle" -> strategy = new SquiggleStrategy(paintModel, paintPanel);
-                        case "Square" -> strategy = new SquareStrategy(paintModel, paintPanel);
-                        case "Polyline" -> strategy = new PolylineStrategy(paintModel, paintPanel);
-                        case "Triangle"  -> strategy = new TriangleStrategy(paintModel, paintPanel);
-                        case "Oval" -> strategy = new OvalStrategy(paintModel, paintPanel);
-                        default -> System.out.println("Unknown tool: " + mode);
-                }
-
-                if (strategy != null) {
-                        paintPanel.setStrategy(strategy);
-                        System.out.println("Tool: " + strategy.getName());
-                }
-        }
+        // setMode(String) removed now handled via ShapeChooserPanel.
 
         private MenuBar createMenuBar() {
-
                 MenuBar menuBar = new MenuBar();
-                Menu menu;
-                MenuItem menuItem;
 
-                // A menu for File
+                // File
+                Menu file = new Menu("File");
+                MenuItem newItem  = new MenuItem("New");
+                MenuItem openItem = new MenuItem("Open");
+                MenuItem saveItem = new MenuItem("Save");
+                file.getItems().addAll(newItem, openItem, saveItem, new SeparatorMenuItem());
 
-                menu = new Menu("File");
+                MenuItem exitItem = new MenuItem("Exit");
+                exitItem.setOnAction(e -> Platform.exit());
+                file.getItems().add(exitItem);
 
-                menuItem = new MenuItem("New");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
+                Menu edit = new Menu("Edit");
+                MenuItem cutItem   = new MenuItem("Cut");
+                MenuItem copyItem  = new MenuItem("Copy");
+                MenuItem pasteItem = new MenuItem("Paste");
+                MenuItem undoItem  = new MenuItem("Undo");
+                MenuItem redoItem  = new MenuItem("Redo");
 
-                menuItem = new MenuItem("Open");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
+                // replace when implemented
+                cutItem.setOnAction(e -> System.out.println("Cut (not implemented)"));
+                copyItem.setOnAction(e -> System.out.println("Copy (not implemented)"));
+                pasteItem.setOnAction(e -> System.out.println("Paste (not implemented)"));
+                undoItem.setOnAction(e -> System.out.println("Undo (not implemented)"));
+                redoItem.setOnAction(e -> System.out.println("Redo (not implemented)"));
+                edit.getItems().addAll(cutItem, copyItem, pasteItem, new SeparatorMenuItem(),
+                        undoItem, redoItem);
 
-                menuItem = new MenuItem("Save");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menu.getItems().add(new SeparatorMenuItem());
-
-                menuItem = new MenuItem("Exit");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menuBar.getMenus().add(menu);
-
-                // Another menu for Edit
-
-                menu = new Menu("Edit");
-
-                menuItem = new MenuItem("Cut");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menuItem = new MenuItem("Copy");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menuItem = new MenuItem("Paste");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menu.getItems().add(new SeparatorMenuItem());
-                menuItem = new MenuItem("Undo");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menuItem = new MenuItem("Redo");
-                menuItem.setOnAction(this);
-                menu.getItems().add(menuItem);
-
-                menuBar.getMenus().add(menu);
-
+                menuBar.getMenus().addAll(file, edit);
                 return menuBar;
         }
-
-
-        @Override
-        public void handle(ActionEvent event) {
-                System.out.println(((MenuItem) event.getSource()).getText());
-                String command = ((MenuItem) event.getSource()).getText();
-                System.out.println(command);
-                if (command.equals("Exit")) {
-                        Platform.exit();
-                }
-        }
-
 }
