6c0e4cb8f9b49c0e7d1c2d0f713f062c4f94588b
Anushka Roy <anushka.roy@mail.utoronto.ca>
2025-11-05 20:06:07 -0500

    update US3.009

---
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/CopyStrategy.java
@@ -0,0 +1,30 @@
+package ca.utoronto.utm.assignment2.paint;
+
+import javafx.scene.input.MouseEvent;
+
+public class CopyStrategy implements ToolStrategy {
+    private final PaintModel model;
+
+    public CopyStrategy(PaintModel model) {
+        this.model = model;
+    }
+
+    @Override
+    public String getName() {
+        return "Copy";
+    }
+
+    @Override
+    public void onMousePressed(MouseEvent e) {
+        Shape clicked = model.findTopmostAt(e.getX(), e.getY());
+        if (clicked != null) {
+            model.setSelectedShape(clicked);
+            model.copyShape();
+        }
+    }
+
+    @Override public void onMouseDragged(MouseEvent e) { }
+    @Override public void onMouseReleased(MouseEvent e) { }
+    @Override public void onMouseMoved(MouseEvent e) { }
+    @Override public void onMouseClicked(MouseEvent e) { }
+}
